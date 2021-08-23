fa993e131fe3f2ab40a97d4e41e919abee4d2eb3
Tejas <73345215+Tejas1004@users.noreply.github.com>
2021-08-23 20:36:02 +0530

    prograd id 5139

--- app.js
+++ app.js
@@ -9,23 +9,34 @@ const modifyFoods = ["pizza", "burger", "fingerChips", "donuts", "springRoll"];
 modifyFoods.splice(2,0,"noodles","icecream");
 console.log(modifyFoods);
 
+//functio #3:Filter
+const numberArray = [12,324,213,4,2,3,45,4234];
+const iseven = numberArray.filter((item => item % 2 == 0 ));
+console.log(iseven);
 
-//Function #3: Filter
-var numberArray = [12,324,213,4,2,3,45,4234];
-isEven = numberArray.filter((item) => item % 2 == 0  );
-console.log(isEven);
+//Function #4: Reject
+let nonPrimeArr = numberArray.filter(nonPrime = (number) =>
+{
+    for(let i = 2;i<=Math.sqrt(number);i++)
+    {
+        if(number%i == 0)
+        {
+            return number;
+        }
+    }
+})
+console.log(nonPrimeArr);
+//Function #5: Lambda
+let evenArr2 = numberArray.filter((number)=> number%2 === 0)
+console.log(evenArr2);
 
-//Function #4: Lambda
-var numberArray = [12, 324, 213, 4, 2, 3, 45, 4234];
-isEven = numberArray.filter((item) => item % 2 == 0  );
-console.log(isEven);
-
-//Function #5: Map
+//Function #6: Map
 var myArray = [11, 34, 20, 5, 53, 16];
-findSquareOfNumbers = myArray.map((item) => item * item ) ;
+const findSquareOfNumbers= myArray.map((item => item*2));
 console.log(findSquareOfNumbers);
 
-//Function #6: Reduce
-var myArray = [2, 3, 5, 10];
-var multiply = myArray.reduce((init, item) => init * item , 1);
+//Function #7: Reduce
+var myArray= [2, 3, 5, 10];
+var multiply = myArray.reduce((accumaltor,currentvalue) => accumaltor*currentvalue,1);
 console.log(multiply);
+
